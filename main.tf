515f24d005212d8e1596fd44c572e763b8c8dce1
padlichan <lili.berenyi@gmail.com>
2026-01-28 12:08:17 +0000

    Fix broken security group id

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   subnet_id              = "subnet-ba3fc8d3"
-  vpc_security_group_ids = ["sg-00d5a1d67232f43a4"]
+  vpc_security_group_ids = ["sg-0d951b9dddd0a3768"]
   key_name               = "terraform-key"
   user_data = templatefile("${path.module}/cloud-init.yml.tmpl", {
     instance_name = "duties-api-lili"
