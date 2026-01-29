752d83b07494780e6d0093f522df88a5f58e9150
padlichan <lili.berenyi@gmail.com>
2026-01-29 12:21:04 +0000

    Refactor: Dynamically getting default vpc and subnet
    Co-authored-by: Lawrence <acodeninja@users.noreply.github.com>

---
+++ infrastructure/application/main.tf
@@ -0,0 +1,77 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+data "aws_vpcs" "default" {
+  filter {
+    name   = "is-default"
+    values = ["true"]
+  }
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = data.aws_vpcs.default[*].id
+  }
+}
+
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.ssh_key.private_key_pem
+  filename = "./.ssh/terraform_rsa"
+}
+
+resource "local_file" "public_key" {
+  content  = tls_private_key.ssh_key.public_key_openssh
+  filename = "./.ssh/terraform_rsa.pub"
+}
+
+resource "aws_security_group" "this" {
+  name   = "lili-app-security-group"
+  vpc_id = data.aws_vpcs.default[0].id
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = data.aws_subnets.default[0].id
+  vpc_security_group_ids = [aws_security_group.this.id]
+  key_name               = "terraform-key"
+  user_data = templatefile("${path.module}/cloud-init.yml.tmpl", {
+    instance_name   = "duties-api-lili"
+    docker_username = var.docker_username
+    database_url    = var.database_url
+  })
+}
