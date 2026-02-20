14bf8124ea1f13baa338308492ccabd82a7f86c2
padlichan <lili.berenyi@gmail.com>
2026-02-20 13:15:47 +0000

    change frontend app port back to default

--- frontend/app.py
+++ frontend/app.py
@@ -43,4 +43,4 @@ def index():
     return render_template('index.html', duties=duties, error_message=error_message)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
